1d7d70a04f0b088567acde65bb11be846a153f30
BlasQu <damianhs138@gmail.com>
2021-03-17 13:22:45 +0100

    Testing notification with pending intents and buttons

--- app/src/main/java/com/example/notificationswithbuttons/MainActivity.kt
+++ app/src/main/java/com/example/notificationswithbuttons/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.notificationswithbuttons
 
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
@@ -14,11 +16,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
         val mNotification = NotificationCompat.Builder(this, DIManager.channelID)
             .setSmallIcon(R.drawable.ic_android_white_24dp)
             .setContentText("Try out these new, sweet buttons!")
             .setContentTitle("Notification with buttons.")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .addAction(R.drawable.ic_android_white_24dp, "Test", pendingIntent)
             .build()
 
         NotificationManagerCompat.from(this).notify(1, mNotification)
